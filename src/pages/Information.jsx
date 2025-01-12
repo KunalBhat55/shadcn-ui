2d717ebfa5bea4df7c116ac10f9c9822a17da246
Kunal Bhat <kunalbhat6767@gmail.com>
2025-01-12 22:16:04 +0530

    refactor Information page layout; enhance tab styling and structure for better user experience

--- src/pages/Information.jsx
+++ src/pages/Information.jsx
@@ -4,48 +4,77 @@ import ClientInfoCard from "../components/ClientInfoCard";
 import { clientData, productData, employeeData } from "../data";
 
 function Information() {
- 
   return (
-    <div className="flex justify-center pr-4 space-x-16">
-      <Tabs defaultValue="account" className="w-[65rem]">
-
-        <TabsList className="flex space-x-24 ">
-          <TabsTrigger value="account" className="px-4 py-2">
-            Account
-          </TabsTrigger>
-          <TabsTrigger value="password" className="px-4 py-2">
-            Password
-          </TabsTrigger>
-          <TabsTrigger value="settings" className="px-4 py-2">
-            Settings
-          </TabsTrigger>
-          <TabsTrigger value="notifications" className="px-4 py-2">
-            Notifications
-          </TabsTrigger>
-          <TabsTrigger value="privacy" className="px-4 py-2">
-            Privacy
-          </TabsTrigger>
+    <div className="flex justify-center px-4 py-8 bg-gray-100 min-h-screen">
+      <Tabs
+        defaultValue="account"
+        className="w-full max-w-6xl bg-white rounded-lg shadow-md"
+      >
+        <TabsList className="flex justify-between border-b border-gray-200 p-4">
+          {[
+            { value: "account", label: "Account" },
+            { value: "password", label: "Password" },
+            { value: "settings", label: "Settings" },
+            { value: "notifications", label: "Notifications" },
+            { value: "privacy", label: "Privacy" },
+          ].map((tab) => (
+            <TabsTrigger
+              key={tab.value}
+              value={tab.value}
+              className="px-6 py-2 text-gray-600 transition-all duration-300 rounded hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 data-[state=active]:text-blue-600 data-[state=active]:font-semibold data-[state=active]:border-b-2 data-[state=active]:border-blue-500"
+            >
+              {tab.label}
+            </TabsTrigger>
+          ))}
         </TabsList>
 
-        {/* Tabs Content goes here */}
-        <TabsContent className="mt-12 mr-64 min-h-80" value="account">
-          <ClientInfoCard clientData={clientData} />
-        </TabsContent>
-        <TabsContent value="password" className="p-4">
-          <ClientInfoCard clientData={productData} />
-        </TabsContent>
-        <TabsContent value="settings" className="p-4">
-          <ClientInfoCard clientData={employeeData} />
-        </TabsContent>
-        <TabsContent value="notifications" className="p-4">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-          quidem.
-        </TabsContent>
-        <TabsContent value="privacy" className="p-4">
-          <Calendar />
-        </TabsContent>
+        <div className="p-6">
+          {/* Account Tab */}
+          <TabsContent
+            className="rounded-lg shadow-md bg-gray-50 p-4"
+            value="account"
+          >
+            <ClientInfoCard
+             
+            clientData={clientData} />
+            <ClientInfoCard
+             
+            clientData={clientData} />
+            
+          </TabsContent>
+          {/* Password Tab */}
+          <TabsContent
+            className="rounded-lg shadow-md bg-gray-50 p-4"
+            value="password"
+          >
+            <ClientInfoCard clientData={productData} />
+          </TabsContent>
+          {/* Settings Tab */}
+          <TabsContent
+            className="rounded-lg shadow-md bg-gray-50 p-4"
+            value="settings"
+          >
+            <ClientInfoCard clientData={employeeData} />
+          </TabsContent>
+          {/* Notifications Tab */}
+          <TabsContent
+            className="rounded-lg shadow-md bg-gray-50 p-4"
+            value="notifications"
+          >
+            <p className="text-gray-700">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+              quidem.
+            </p>
+          </TabsContent>
+          {/* Privacy Tab */}
+          <TabsContent
+            className="rounded-lg shadow-md bg-gray-50 p-4"
+            value="privacy"
+          >
+            <Calendar />
+          </TabsContent>
+        </div>
       </Tabs>
-    
     </div>
   );
 }
